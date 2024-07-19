5cdea9d1b8a6f2ef6788efe2ce3f29a89c8e6bc1
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-07-19 17:34:21 +0900

    ✨ feat : 11 템플릿 코드

--- pages/boards/[id].tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router';
-
-export default function PostView() {
-  const router = useRouter();
-  const id = router.query['id'];
-
-  return <div>{id} post 페이지</div>;
-}
